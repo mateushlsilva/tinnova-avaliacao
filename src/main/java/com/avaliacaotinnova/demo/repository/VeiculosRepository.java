981563d662542eedb5d9016e849007c8229e12fb
mateushlsilva <mateushls01@gmail.com>
2025-08-14 19:06:11 -0300

    feat:  Adiciona veiculos repository

---
+++ src/main/java/com/avaliacaotinnova/demo/repository/VeiculosRepository.java
@@ -0,0 +1,9 @@
+package com.avaliacaotinnova.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.avaliacaotinnova.demo.entity.Veiculos;
+
+public interface VeiculosRepository extends JpaRepository<Veiculos, Long>{
+
+}
