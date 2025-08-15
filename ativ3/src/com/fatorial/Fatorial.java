e85feefff931060c02a2a1fe42b5fa2143956358
mateushlsilva <mateushls01@gmail.com>
2025-08-15 15:49:42 -0300

    feat: Adiciona atividade 3 fatorial

---
+++ ativ3/src/com/fatorial/Fatorial.java
@@ -0,0 +1,12 @@
+package com.fatorial;
+
+public class Fatorial {
+    public static long calcFatorial(int numero){
+        if (numero <= 1) return 1;
+        return numero * calcFatorial(numero -1);
+    } 
+    public static void main(String[] args) {
+        int numero = 5;
+        System.out.println("Fatorial de " + numero + "! é " + calcFatorial(numero));
+    }
+}
